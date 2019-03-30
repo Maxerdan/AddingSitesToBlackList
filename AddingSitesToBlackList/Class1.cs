3ba697f1a4872f8034415e509eeae6fd1c3d0084
Maxerdan <48237187+Maxerdan@users.noreply.github.com>
2019-03-30 21:23:17 +0500

    Ready new Theme

--- AddingSitesToBlackList/Class1.cs
+++ AddingSitesToBlackList/Class1.cs
@@ -3,8 +3,89 @@ using System.Collections.Generic;
 
 namespace AddingSitesToBlackList
 {
+    /// <summary>
+    /// Добавление сайтов в черный список Единого Реестра
+    /// </summary>
     public class AddingSitesToBlackList
     {
-         
+        /// <summary>
+        /// Список сайтов
+        /// </summary>
+        public List<Sites> Sites { get; set; }
+
+        /// <summary>
+        /// Статус сайта
+        /// </summary>
+        public Status Status { get; set; }
+
+        /// <summary>
+        /// Дата блокировки
+        /// </summary>
+        public DateTime BlockDate { get; set; }
+    }
+
+    /// <summary>
+    /// Возможные статусы
+    /// </summary>
+    public enum Status
+    {
+        /// <summary>
+        /// Работает
+        /// </summary>
+        run,
+
+        /// <summary>
+        /// Заблокирован
+        /// </summary>
+        blocked,
+
+        /// <summary>
+        /// На рассмотрении
+        /// </summary>
+        notSolved
+    }
+
+    /// <summary>
+    /// Класс сайтов
+    /// </summary>
+    public class Sites
+    {
+        /// <summary>
+        /// Адрес сайта
+        /// </summary>
+        public string Adress { get; set; }
+        
+        /// <summary>
+        /// Адрес публикации
+        /// </summary>
+        public string AdressPubl { get; set; }
+
+        /// <summary>
+        /// Причина
+        /// </summary>
+        public string Reason { get; set; }
+
+        /// <summary>
+        /// Список нарушений
+        /// </summary>
+        public List<string> Violations = new List<string>()
+        {
+            "Экстримизм", "Угрозы к свержению власти", "Нарушение авторских прав", "Нарушение тайны частной жизни", "Раскрытие гостайны"
+        };
+        
+        /// <summary>
+        /// Дата отправки 
+        /// </summary>
+        public DateTime SendingDay { get; set; }
+
+        /// <summary>
+        /// Возможная дата нарушения
+        /// </summary>
+        public DateTime PublishDate { get; set; }
+
+        /// <summary>
+        /// Скриншот прилагаемый
+        /// </summary>
+        public byte[] Screenshot { get; set; }
     }
 }
